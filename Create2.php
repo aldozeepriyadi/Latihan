c91457ba3cc6d6ed4756cbe190b7464b75d1e964
aldozeepriyadi <aldo.fernandosupriyadi@gmail.com>
2022-10-07 09:30:30 +0700

    Penambahan file

---
+++ Create2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+      <title>Input Data Pegawai</title>
+</head>
+<body>
+    <form method="post" action= "Create.php">
+	   id_pegawai  : <input type="text" name="id_pegawai">
+	   <br>
+	   <br>
+	   nama_pegawai : <input type="text" name="nama_pegawai">
+	   <br>
+	   <br>
+       NPWP : <input type="text" name="npwp">
+	   <br>
+	   <br>
+       no_telp : <input type="text" name="no_telp">
+	   <br>
+	   <br>
+       alamat : <input type="text" name="alamat">
+	   <br>
+	   <br>
+	   <input type="submit" name="Simpan" value="Simpan">
+	</form>
+</body>
+</html>
